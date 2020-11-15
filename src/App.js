91813011b2f65b0b72f8636ea6a4973e74496cee
xavier-breuil <breuilxavier@gmail.com>
2020-11-15 19:31:05 +0100

    dev: Handle http errors with alert.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import {Component} from 'react';
 
+import Alert from 'react-bootstrap/Alert';
+
 import './App.css';
 import backend from './apiConf.js';
+import appTexts from './texts.js';
 
 /**
 App component.
@@ -9,20 +12,46 @@ App component.
 @returns {Component} - App component
 */
 class AppComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   componentDidMount() {
     backend.get('conferences/')
       .then(resp => {
-        console.log(resp);
+        this.setState({meetingList: resp.data.results[0]});
       })
       .catch(err => {
+        // TODO: implement way to warn admin
         console.log(err);
+        this.setState({
+          alert: {
+            text: appTexts.loadingError,
+            variant: 'danger'
+          }
+        });
       });
   }
 
+  generateAlert() {
+    return (
+      this.state.alert &&
+        <Alert
+          variant={this.state.alert.variant}
+          onClose={() => this.setState({alert: undefined})}
+          dismissible>
+          {this.state.alert.text}
+        </Alert>
+    );
+  }
+
   render () {
+    const alert = this.generateAlert();
+
     return (
       <div className="App">
+        {alert}
         <header className="App-header">
           Search meetings
         </header>
---
+++ src/texts.js
@@ -0,0 +1,6 @@
+/* eslint-disable max-len */
+const appTexts = {
+  loadingError: 'Il semblerait qu\'il y ai un problème de chargement des données. Veuillez réessayer plus tard.'
+};
+
+export default appTexts;
