d8b2344decd3eea65b22948922c5c76562c6851f
xavier-breuil <breuilxavier@gmail.com>
2020-11-17 18:05:50 +0100

    conf: Setup chai for test assertions.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,6 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// Use chai and associate plugins for tests.
+import chai from 'chai';
+import createChaiJestDiff from 'chai-jest-diff';
+import dirtyChai from 'dirty-chai';
+
+chai.use(dirtyChai).use(createChaiJestDiff());
---
+++ src/tests/App.test.js
@@ -0,0 +1,7 @@
+import {expect} from 'chai';
+
+describe('App', () => {
+  it('should pass dummy test.', () => {
+    expect(1).to.equal(1);
+  });
+});
