2ef47b9d65b67c5b09aabe5e86191f7ad1b3dec6
xavier-breuil <breuilxavier@gmail.com>
2020-11-17 18:31:37 +0100

    conf: Setup enzyme to test component.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,14 @@
 // Use chai and associate plugins for tests.
+import Adapter from 'enzyme-adapter-react-16';
 import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 import createChaiJestDiff from 'chai-jest-diff';
+import {configure as configureEnzyme} from 'enzyme';
 import dirtyChai from 'dirty-chai';
 
-chai.use(dirtyChai).use(createChaiJestDiff());
+chai
+  .use(dirtyChai)
+  .use(createChaiJestDiff())
+  .use(chaiEnzyme());
+
+configureEnzyme({adapter: new Adapter()});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,11 @@
 import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import Container from 'react-bootstrap/Container';
+import App from '../App.js';
 
 describe('App', () => {
-  it('should pass dummy test.', () => {
-    expect(1).to.equal(1);
+  it('should have a container to display meetings.', () => {
+    const appWrapper = shallow(<App />);
+    expect(appWrapper.find(Container)).to.have.lengthOf(1);
   });
 });
